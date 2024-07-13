abac4004dafe040ae21098f1a67fffb87b3075c5
Luke Macy <lukemacy1@gmail.com>
2024-07-13 16:23:56 -0600

    added custom config and production variables in env file

---
+++ run_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Loading env variables from './.env.local'"
+export DOCKER_COMPOSE_ENV_FILE=./.env.local
+
+echo "killing old docker processes"
+docker compose rm -fs
+
+echo "building docker containers"
+docker compose --env-file $DOCKER_COMPOSE_ENV_FILE -f docker-compose-ssl.yml up --build -d
